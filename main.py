cfa8afd695da07f822852b27dd29a91a7c44f375
Yen-Wen <somvud9843@gmail.com>
2018-03-10 17:37:37 +0800

    TODO: procseeing time based on Resource Profile, and Pre-Resource scheduling

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ import sys
 import time
 import random
 import threading
+import csv
+from operator import attrgetter
 
+def init_output_file():
+    with open('mycsvfile.csv', 'w', newline='') as f:  # Just use 'w' mode in 3.x
+        w = csv.writer(f, delimiter =',',quotechar ="'",quoting=csv.QUOTE_MINIMAL)
+        w.writerow(["f_id", "p_num", "size", "time", "VST", "VFT"])
+
+def save_to_csv(data):
+    with open('mycsvfile.csv', 'a', newline='') as f:  # Just use 'w' mode in 3.x
+        w = csv.writer(f, delimiter =',',quotechar ="'",quoting=csv.QUOTE_MINIMAL)
+        w.writerow([data.f_id, data.p_num, data.size, data.time, data.VST, data.VFT])
 
 class fifo_q(PriorityQueue):
     def __init__(self):
@@ -40,7 +51,7 @@ class Flow(Thread):
         # Bandwidth(bps) 100 * M(2^20)
         self.bw = bw
         # Packet Size(bits)
-        self.p_size = random.randint(160, 400)
+        self.p_size = random.randint(160, 400)*8
         self.stopRequest = threading.Event()
         self.tout = 0
 
@@ -54,8 +65,8 @@ class Flow(Thread):
                 self.tout = 0
 
             # Produce some data
-            time.sleep(self.p_size / self.bw) # transmission delay
-            # time.sleep(1)
+            # time.sleep(self.p_size / self.bw) # transmission delay
+            time.sleep(0.1)
             data = Packet(self.id, p_seq, self.p_size, time.time()-start_time, False)
             p_seq += 1
             self.que.put(data.time, data)
@@ -63,43 +74,42 @@ class Flow(Thread):
         self.que.put(data.time, data)
 
 
-# FIFO queue
+# Queue with resource profile
 class Flow_one(Thread):
 
-    def __init__(self, q, f_id, bw, size):
+    def __init__(self,q, f_id, bw, size, resource_profile):
         Thread.__init__(self)
         self.que = q
-        self.f_id = f_id
+        self.id = f_id
         # Bandwidth(bps) 100 * M(2^20)
         self.bw = bw
         # Packet Size(bits)
-        self.p_size = size #random.randint(160, 400)
+        self.p_size = random.randint(160, 400)*8
+        self.rp = resource_profile
         self.stopRequest = threading.Event()
         self.tout = 0
-        self.__vt = 0
 
     def run(self):
         p_seq = 0
-        while p_seq < 100:
+        while p_seq < 7:
             if self.tout > 0:
-                print("Flow %d stop sending for %d seconds" % (self.f_id, self.tout))
-                time.sleep(self.tout)
-                self.tout = 0
+                self.sleep()
+
             # Produce some data
-            # print(self.f_id, "  Delay = ", self.p_size / self.bw)
-            time.sleep(self.p_size / self.bw) # transmission delay
-            data = Packet(self.f_id, p_seq, self.p_size, time.time()-start_time, False)
+            # time.sleep(self.p_size / self.bw) # transmission delay
+            time.sleep(0.1)
+            data = Packet(self.id, p_seq, self.p_size, time.time()-start_time, False)
+            setattr(data, "rp", self.rp)
             p_seq += 1
             self.que.put(data.time, data)
-
-        data = Packet(self.f_id, p_seq, self.p_size, time.time()-start_time, True)
+        data = Packet(self.id, p_seq, self.p_size, time.time()-start_time, True)
+        setattr(data, "rp", self.rp)
         self.que.put(data.time, data)
 
-    def setvt(self, t):
-        self.__vt = t
-
-    def getvt(self):
-        return self.__vt
+    def sleep(self):
+        print("Flow %d stop sending for %d seconds" % (self.id, self.tout))
+        time.sleep(self.tout)
+        self.tout = 0
 
 
 # A thread that consumes data
@@ -126,7 +136,8 @@ def consumer(fList):
     
 def pq(q):
     f_flow = 0  # Finished flow counter
-    sys_VT = 0
+    global sys_VT
+    global actFL
     actFL={}
     while f_flow != f_num:
         data = q.get()
@@ -141,55 +152,95 @@ def pq(q):
         actFL[data.f_id].pVFT = data.VFT
         actFL[data.f_id].put(data)
         
-        sys_VT = data.VST
+        
         print(data.__dict__)
+        # save_to_csv(data)
         sys.stdout.flush()
-        time.sleep(0.001)
         # print("Done", "There is %d packets in queue." % f_que.que.qsize())
         if data.isLast:
             f_flow += 1
 
 #Calculate Packet i's Dominatent Resource porcessing time
 def drpt(pkt):
+    DR = max(pkt.rp)
+    return DR
+    # Fix demand
+    '''
     n = pkt.f_id
     return{
         1:4,
         2:3,
     }[n]
+    '''
+def r1():
+    global sys_VT
+    time.sleep(0.000001)
+    idle = True
+    while True:
+        data = []
+        for keys in actFL:
+            if not actFL[keys].empty():
+                data.append(list(actFL[keys].queue)[0])
+        data = sorted(data, key= attrgetter("f_id"))
+        try:
+            next_packet = min(data,key=attrgetter("VST"))
+            idle = True
+        except ValueError:
+            if idle:
+                idle = False
+                print("Resource_1 idle...")
+                sys.stdout.flush()
+            continue
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            break
+        # Remove packet i in flow i's queue 
+        actFL[next_packet.f_id].get()
+        print("Produce packet:%d-%d" % (next_packet.f_id,next_packet.p_num))
+        print("Update System Virtual Time: %d -> %d" % (sys_VT, next_packet.VST))
+        sys_VT = next_packet.VST
+        sys.stdout.flush()
+        # Processing time for packet
+        time.sleep(0.25)
+            # if actFL[next_packet.f_id].empty():
+            #     del actFL[next_packet.f_id]
+    
+if __name__ == "__main__":
+    # Create the shared queue and launch both threads
+    start_time = time.time()
+    f_num = 2
+    sys_VT = 0
+    # init_output_file()
+    q = fifo_q()
+    tList=[]
+    rp = [
+        [4, 1],
+        [1, 3]
+    ]
+    for i in range(1, f_num+1):
+        if 1:
+            t = Flow_one(q, i, 200*2**20, 512, rp[i-1])
+        else:
+            t = Flow(q, i, 200*2**20, 512)
+        t.start()
+        tList.append(t)
     
 
-# Create the shared queue and launch both threads
+    # Run the consumer to dequeue
+    t1 = Thread(target=pq, args=(q,))
+    t1.start()
 
-global f_num
-global start_time
+    r = Thread(target=r1)
+    r.start()
+    # Make flow stop sending for x seconds. (tout = timeout)
+    time.sleep(0.4)
+    tList[1].tout=2
+    t1.join()
 
-# System's virtual time
-start_time = time.time()
-f_num = 2
+    time.sleep(7)
 
-# t = Flow(1, 100*2**20, 512)
-# t.start()
-# t.join
-
-q = fifo_q()
-tList=[]
-for i in range(1, f_num+1):
-    t = Flow(q, i, 100*2**10, 512)
-    # t = Flow_one(q, i, 100*2**10, 512)
-    t.start()
-    tList.append(t)
+    for t in tList:
+        t.stopRequest.set()
 
 
-# Run the consumer to dequeue
-t1 = Thread(target=pq, args=(q,))
-t1.start()
-# Make flow stop send for x second. tout = timeout
-# tList[0].tout=5
 
-t1.join()
-
-
-time.sleep(2)
-
-for t in tList:
-    t.stopRequest.set()
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from queue import Queue
 import time
+from operator import attrgetter
 
 class Packet:
 
@@ -7,12 +8,23 @@ class Packet:
         self.flow_id = flow_id
         self.p_num = p_num
         self.size = size
-        self.time = g_time
+        self.arr_time = g_time
         self.isLast = isLast
 
-q = Queue()
-for i in range(1,10):
-    p = Packet(1, i, 123, time.time(), False)
-    q.put(p)
-data = list(q.queue)[0]
-print(data)
+# q = Queue()
+def main():
+    qDict = {}
+    data = []
+    for i in range(1,3):
+        qDict[i] = Queue()
+        for j in range(1,10):
+            p = Packet(1, j, 123, time.time(), False)
+            qDict[i].put(p)
+    print(qDict[1])
+    for i in qDict:
+        data.append(list(qDict[i].queue)[0])
+        # print(data.__dict__)
+    print( min(data,key=attrgetter("arr_time")).p_num)
+
+if __name__ == "__main__":
+    main()
